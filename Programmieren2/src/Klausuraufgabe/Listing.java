2f8cf362556a00c38293f532a7e6c88182fc2a66
mfg94 <michael.gutmair@freenet.de>
2016-06-09 09:38:42 +0200

    kleine KLausuraufgabe

---
+++ Programmieren2/src/Klausuraufgabe/Listing.java
@@ -0,0 +1,12 @@
+package Klausuraufgabe;
+
+public class Listing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringFunction f1 = (s, cs1,  cs2) -> s.replace(cs1, cs2);
+
+	}
+
+}
---
+++ Programmieren2/src/Klausuraufgabe/StringFunction.java
@@ -0,0 +1,7 @@
+package Klausuraufgabe;
+
+public interface StringFunction {
+	
+	public String doSomething(String s, CharSequence cs1, CharSequence cs2);
+
+}
