624974ae92a07ecd8a0ce2976ce66bbfcf2b1122
mfg94 <michael.gutmair@freenet.de>
2016-06-17 18:27:07 +0200

    Stoppuhr funktioniert.... Dirty....

--- Programmieren2/src/kapitel8/GUIMain.java
+++ Programmieren2/src/kapitel8/GUIMain.java
@@ -12,36 +12,75 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GUIMain {
+public class GUIMain extends JFrame implements ActionListener {
+
+	JPanel panel;
+	JButton start;
+	JButton stop;
+	JButton reset;
+	
+
+	Stoppuhr timer;
+
+	public GUIMain() {
+		this.setTitle("Stoppuhr");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(600, 200);
+
+		panel = new JPanel();
+		panel.setLayout(null);
+
+		timer = new Stoppuhr();
+
+		start = new JButton("Start");
+		start.setBounds(0, 50, 100, 50);
+		start.addActionListener(this);
+
+		stop = new JButton("Stop");
+		stop.setBounds(100, 50, 100, 50);
+		stop.addActionListener(this);
+		stop.setEnabled(false);
+		
+		reset = new JButton("Reset");
+		reset.setBounds(200,50,100,50);
+		reset.addActionListener(this);
+		reset.setEnabled(false);
+
+		panel.add(timer);
+		panel.add(start);
+		panel.add(stop);
+		panel.add(reset);
+
+		this.add(panel);
+		panel.setVisible(true);
+
+	}
 
 	public static void main(String[] args) {
-		JFrame stopWatch = new JFrame("StopWatch");
-		stopWatch.setDefaultCloseOperation(3);
-		stopWatch.setSize(600, 300);
 
-		Container c = stopWatch.getContentPane();
-		c.setLayout(null);
+		GUIMain main = new GUIMain();
+		main.setVisible(true);
 
-		Stoppuhr timer = new Stoppuhr(100.00234);
-		JButton start = new JButton("Start");
-		start.setBounds(50, 50, 150, 80);
+	}
 
-		c.add(start);
-		c.add(timer);
-
-		start.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				timer.count();
-				start.
-
-			}
-		});
-
-		c.setVisible(true);
-		stopWatch.setVisible(true);
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == start) {
+			timer.count();
+			start.setEnabled(false);
+			reset.setEnabled(true);
+			stop.setEnabled(true);
+		} else if (e.getSource() == stop) {
+			timer.stop();
+			start.setEnabled(true);
+			stop.setEnabled(false);
+		} else if(e.getSource()==reset){
+			timer.reset();
+			reset.setEnabled(false);
+			stop.setEnabled(false);
+			start.setEnabled(true);
+			
+		}
+		
 	}
 
 }
--- Programmieren2/src/kapitel8/Stoppuhr.java
+++ Programmieren2/src/kapitel8/Stoppuhr.java
@@ -14,18 +14,20 @@ import java.text.DecimalFormat;
 import javax.swing.*;
 import javax.swing.Timer;
 
-public class Stoppuhr extends JPanel {
+public class Stoppuhr extends JPanel implements ActionListener {
 
 	final int delay = 100;
 	double seconds = 0.0;
 	final DecimalFormat df = new DecimalFormat("#0.0");
 	final TitledBorder border = new TitledBorder(new LineBorder(Color.BLUE, 1), "Display");
+	Timer t;
 
-	public Stoppuhr(double seconds) {
+	public Stoppuhr() {
 		this.setOpaque(false);
 		this.setBorder(border);
 		this.setBounds(0, 0, 200, 50);
 		border.setTitleColor(Color.BLUE);
+		t = new Timer(delay, this);
 	}
 
 	public void paintComponent(Graphics g) {
@@ -38,10 +40,23 @@ public class Stoppuhr extends JPanel {
 	}
 
 	public void count() {
+		this.t.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
 		this.seconds += 0.1;
 		this.repaint();
 	}
 
+	public void stop() {
+		this.t.stop();
+	}
 
+	public void reset() {
+		this.seconds = 0;
+		this.stop();
+		this.repaint();
+	}
 
 }
